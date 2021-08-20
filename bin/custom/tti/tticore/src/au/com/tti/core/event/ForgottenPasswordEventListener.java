3c53d2ed37b84335a6857b0eb7f85ced9038e959
Sandeep Kalpane <skalpane@dxc.com>
2021-08-20 11:35:11 +1000

    Revert

--- bin/custom/tti/tticore/src/au/com/tti/core/event/ForgottenPasswordEventListener.java
+++ bin/custom/tti/tticore/src/au/com/tti/core/event/ForgottenPasswordEventListener.java
@@ -68,7 +68,6 @@ public class ForgottenPasswordEventListener extends AbstractAcceleratorSiteEvent
 		forgottenPasswordProcessModel.setStore(event.getBaseStore());
 		getModelService().save(forgottenPasswordProcessModel);
 		getBusinessProcessService().startProcess(forgottenPasswordProcessModel);
-		String name = "Sandeep";
 	}
 
 	@Override
